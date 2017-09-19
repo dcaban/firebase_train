d9ddcff847a7469e64700d593fd0d587c64a97ee
dcaban <davidjcaban@gmail.com>
2017-09-19 12:02:23 -0400

    Front end done

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyBcpqzhLTHrdQZXp47vze6fN-opRanj0dQ",
+    authDomain: "train-time-80754.firebaseapp.com",
+    databaseURL: "https://train-time-80754.firebaseio.com",
+    projectId: "train-time-80754",
+    storageBucket: "",
+    messagingSenderId: "641507768536"
+};
+firebase.initializeApp(config);
+
+//Button for adding trains
+
+$("#add-train-btn").on("click", function (event) {
+    event.preventDefault();
+
+    //Grabs user input
+    var trName = $("#train-name-input")
+});
